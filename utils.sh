694f7e6e1061a2bc0eb0a8083871b9764d1f4c47
Sen Zhang <szhang.hust@gmail.com>
2019-02-11 18:55:06 +0800

    add smart install software for different system

--- utils.sh
+++ utils.sh
@@ -1,13 +1,36 @@
 #!/usr/bin/env bash
 
+case $OSTYPE in
+    cygwin*)
+        __INSTALL__="__cygwin_install "
+        ;;
+    msys*)
+        __INSTALL__="pacman -Sy "
+        ;;
+    linux*)
+        #todo
+        ;;
+    darwin*)
+        __INSTALL__="brew install "
+        ;;
+esac
+
+__cygwin_install() {
+    echo "Sorry, cygwin can not install [$1] automatically, Please install by cygwin installer"
+}
+
+smart_install() {
+    $__INSTALL__ $1
+}
+
 command_not_installed() {
-  echo "Check if [$1] installed..."
-  found=$(which $1 2>/dev/null)
-  if [ -z "$found" ]; then
-    echo -e "\t------- No"
-    return 0
-  else
-    echo -e "\t------- Yes"
-    return 1
-  fi
+    echo "Check if [$1] installed..."
+    found=$(which $1 2>/dev/null)
+    if [ -z "$found" ]; then
+        echo -e "\t------- No"
+        return 0
+    else
+        echo -e "\t------- Yes"
+        return 1
+    fi
 }
--- zsh/install.sh
+++ zsh/install.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
-echo -e "[Check] \033[32mzsh\033[0m already configured?"
+base_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+source $base_dir/../utils.sh
+
+if command_not_installed zsh; then
+    smart_install zsh
+fi
+
+echo -e "[Check] \033[32mzsh\033[0m configured by oh-my-zsh?"
 
 if [ -d "$ZSH" ]; then
-    echo -e "Installed.\n"
+    echo -e "Configured.\n"
 else
-    echo -e "Not Installed.\n"
+    echo -e "Not Configured.\n"
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
