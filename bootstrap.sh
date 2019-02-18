0a216a6e25c66b83b2cde5ae3b879dde565b5ee9
Sen Zhang <szhang.hust@gmail.com>
2019-02-18 23:00:19 +0800

    do some config before install

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,58 @@
 #!/bin/bash -i
 
-if [ -z $(which git 2>/dev/null) ]; then
-    printf "\033[32mgit\033[0m need to be installed first!\n"
-    exit 233
-fi
+init_install_config() {
+    case $OSTYPE in
+        cygwin*)
+            if [ -z $(which git 2>/dev/null) ]; then
+                printf "\033[32mgit\033[0m need to be installed first!\n"
+                exit 233
+            fi
+            ;;
+        darwin*)
+            #install brew
+            if ! command -v brew >/dev/null 2>&1; then
+                printf "Brew is not installed! Install brew...\n"
+                /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+            fi
+
+            echo "Change mirrors for Homebrew"
+            #see https://mirrors.tuna.tsinghua.edu.cn/help/homebrew/
+            cd "$(brew --repo)"
+            git remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git
+
+            cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
+            git remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git
+
+            export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles
+            # Make sure we’re using the latest Homebrew.
+            echo "brew update"
+            brew update
+
+            # Upgrade any already-installed formulae.
+            echo "brew upgrade"
+            brew upgrade
+
+            brew install git
+            ;;
+        msys*)
+            cp /etc/pacman.d/mirrorlist.mingw32 /etc/pacman.d/mirrorlist.mingw32.bak
+            cp /etc/pacman.d/mirrorlist.mingw64 /etc/pacman.d/mirrorlist.mingw64.bak
+            cp /etc/pacman.d/mirrorlist.msys /etc/pacman.d/mirrorlist.msys.bak
+            echo 'Server = https://mirrors.tuna.tsinghua.edu.cn/msys2/mingw/i686' > /etc/pacman.d/mirrorlist.mingw32
+            echo 'Server = https://mirrors.tuna.tsinghua.edu.cn/msys2/mingw/x86_64' > /etc/pacman.d/mirrorlist.mingw64
+            echo 'Server = https://mirrors.tuna.tsinghua.edu.cn/msys2/msys/$arch' > /etc/pacman.d/mirrorlist.msys
+
+            pacman -Syyu
+            paman -Sy git
+            ;;
+        *)
+            echo "Error: system is not supported yet :("
+            exit -1
+            ;;
+    esac
+}
+
+init_install_config
 
 ETC=$HOME/.local/etc
 BIN=$HOME/.local/bin
