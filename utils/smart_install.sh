d68a034e85edf6ecaaa1b633c62234cf0b76d63f
Sen Zhang <szhang.hust@gmail.com>
2019-03-06 22:05:38 +0800

    add missing change for last commit

--- utils/smart_install.sh
+++ utils/smart_install.sh
@@ -7,6 +7,9 @@ else
     return
 fi
 
+base_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source $base_dir/platform.sh
+
 command_not_installed() {
     printf "Check if [$1] installed..."
     found=$(which $1 2>/dev/null)
@@ -19,27 +22,39 @@ command_not_installed() {
     fi
 }
 
-case $OSTYPE in
-    cygwin*)
+command_installed() {
+    printf "Check if [$1] installed..."
+    found=$(which $1 2>/dev/null)
+    if [ -z "$found" ]; then
+        printf "%-24s Not Installed\n"
+        return 1
+    else
+        printf "%-24s     Installed\n"
+        return 0
+    fi
+}
+
+case $SYSTEM in
+    Cygwin*)
         __INSTALL__="apt-cyg install "
         if command_not_installed apt-cyg; then
             git clone https://github.com/transcode-open/apt-cyg.git $HOME/.local
         fi
         ;;
-    msys*)
+    Msys2*)
         __INSTALL__="pacman -Sy "
         ;;
-    linux*)
+    Ubuntu*)
+        __INSTALL__="pacman -Sy "
+        ;;
+    Centos*)
+        __INSTALL__="yum install -y "
         #todo
         ;;
-    darwin*)
+    macOS*)
         __INSTALL__="brew install "
         ;;
 esac
-
-__cygwin_install() {
-    echo "Sorry, cygwin can not install [$@] automatically, Please install by cygwin installer"
-}
 
 smart_install() {
     $__INSTALL__ $@
