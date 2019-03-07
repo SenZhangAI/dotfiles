8fda349c55589fbb793702250d3b0fde29157ff9
Sen Zhang <szhang.hust@gmail.com>
2019-03-07 18:08:11 +0800

    config oh-my-zsh at background, so the process won't be trapped in

--- zsh/install.sh
+++ zsh/install.sh
@@ -8,17 +8,37 @@ if command_not_installed zsh; then
     smart_install zsh
 fi
 
-echo -e "[Check] \033[32mzsh\033[0m configured by oh-my-zsh?"
+config_zsh() {
+    printf "[Check] \033[32mzsh\033[0m configured?"
 
-if [ -d "$HOME/.oh-my-zsh" ]; then
-    echo -e "Configured.\n"
-else
-    echo -e "Not Configured.\n"
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-fi
+    if [ -d "$HOME/.oh-my-zsh" ]; then
+        printf "%-20s Yes.\n"
+    else
+        printf "%-20s No.\n"
+        printf "Configured zsh...\n"
+        {
+            sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+        } &
+    wait
+    fi
+}
 
-if [ ! -f "/etc/passwd" ]; then
-    mkpasswd > /etc/passwd
-fi
+set_zsh_default_shell() {
+    echo "default shell is $SHELL"
+    if [[ $SHELL =~ zsh ]]; then
+        return
+    fi
+    echo "Change defalt shell to zsh..."
+
+    if [ ! -f "/etc/passwd" ]; then
+        mkpasswd > /etc/passwd
+    fi
+
+    #TODO
+}
+
+config_zsh
+
+set_zsh_default_shell
 
 # vim:st=4:sw=4
