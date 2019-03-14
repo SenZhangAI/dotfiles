5a69fbfc59666b9d2852d031f486ff902cdc99fe
Sen Zhang <szhang.hust@gmail.com>
2019-03-14 20:34:28 +0800

    better config for vim

--- vim/install.sh
+++ vim/install.sh
@@ -4,6 +4,8 @@ echo "[Config] .ideavimrc"
 
 base_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
+source $base_dir/../utils/smart_install.sh
+
 case $OSTYPE in
     cygwin*)
         cp $base_dir/ideavimrc $(cygpath -w $USERPROFILE/.ideavimrc)
@@ -19,19 +21,47 @@ case $OSTYPE in
         ;;
 esac
 
+
+check_dependent() {
+    if command_not_installed vim; then
+        smart_install vim
+    fi
+
+    if command_not_installed clang; then
+        smart_install clang
+    fi
+
+    if command_not_installed python2; then
+        smart_install python2
+    fi
+
+    if command_not_installed python3; then
+        smart_install python3
+    fi
+
+    if command_not_installed gcc; then
+        smart_install gcc
+    fi
+}
+
 config_vim() {
-    mkdir -p $HOME/.vim
-    {
-        cd $HOME/.vim
-	    git clone https://github.com/SenZhangAI/vim
-	    ./vim/install.sh
-    } &
+    check_dependent
+
+    vim_dir=$HOME/.vim/vim
+    if [ -d $vim_dir ]; then
+        cd $vim_dir
+        git pull
+    else
+        mkdir -p $HOME/.vim
+        git clone https://github.com/SenZhangAI/vim $vim_dir
+        cd $vim_dir
+    fi
+
+    ./install.sh &
     wait
     return 0
 }
 
-if [ ! -d $HOME/.vim/vim ]; then
-    config_vim
-fi
+config_vim
 
 # vim:st=4:sw=4
