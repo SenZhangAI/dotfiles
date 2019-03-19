beff42a53bd65527a5badedb2e5e450a55fe0c97
Sen Zhang <szhang.hust@gmail.com>
2019-03-19 21:03:29 +0800

    add install emacs

--- emacs/install.sh
+++ emacs/install.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
-echo "[Config] .emacs.d"
 
 base_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source $base_dir/../utils/smart_install.sh
 
+if command_not_installed emacs; then
+    smart_install emacs
+fi
+
+echo "[Config] .emacs.d"
+mkdir -p $HOME/.emacs.d
 cp -r $base_dir/emacs.d/* $HOME/.emacs.d/
 
 # vim:st=4:sw=4
