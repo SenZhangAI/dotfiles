09ac29eeaf7817601143df8a574a00211005f20c
Sen Zhang <szhang.hust@gmail.com>
2019-05-25 04:34:42 +0800

    better discription for git config

--- git/config.sh
+++ git/config.sh
@@ -4,9 +4,9 @@ echo -e "[Check] \033[32mgit global user.email\033[0m configured?"
 
 search=`git config --global user.email`
 
+    echo "[Config] user.email"
 if [ -z "$search" ]; then
     echo -e "Not Found.\n"
-    echo "[Config]"
     git config --global user.email "szhang.hust@gmail.com"
 else
     echo -e "Found.\n"
@@ -14,17 +14,16 @@ fi
 
 echo -e "[Check] \033[32mgit global user.name\033[0m configured?"
 
+echo "[Config] user.name"
 search=`git config --global user.name`
-
 if [ -z "$search" ]; then
-    echo -e "Not Found.\n"
-    echo "[Config]"
     git config --global user.name "Sen Zhang"
 else
     echo -e "Found.\n"
 fi
 
 
+echo "[Config] save username and password"
 # store username and password so I won't need to type username and password again.
 #git config --global credential.helper 'cache --timeout 7200'
 git config --global credential.helper store
