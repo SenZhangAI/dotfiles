28b8ba1db5c7847da695db4b43df775f9b380017
Sen Zhang <szhang.hust@gmail.com>
2019-08-10 17:33:16 +0800

    fix ssh config

--- ssh/config.sh
+++ ssh/config.sh
@@ -8,21 +8,39 @@ if command_not_installed ssh; then
     smart_install openssh
 fi
 
-echo -e "[Check] \033[32mssh\033[0m already configured?"
+printf "[Check] if ssh configured?\t"
 
 search=`cat $HOME/.ssh/id_rsa.pub | grep "szhang.hust@gmail.com"`
 
 if [ -z "$search" ]; then
-    echo -e "Not Found.\n"
+    printf "\t\t\tNot Configured.\n"
+    echo "[Config] add ssh key..."
+    ssh-keygen -t rsa -b 4096 -C "szhang.hust@gmail.com"
+    eval $(ssh-agent -s)
+    ssh-add ~/.ssh/id_rsa
 else
-    echo -e "Found.\n"
-    exit 0
+    printf "\t\t\tConfigured.\n"
 fi
 
-echo "[Config]"
-ssh-keygen -t rsa -b 4096 -C "szhang.hust@gmail.com"
-eval $(ssh-agent -s)
-ssh-add ~/.ssh/id_rsa
-clip < ~/.ssh/id_rsa.pub
 
-# vim:st=4:sw=4
+
+command_found() {
+    if [ -z "$(which $1 2>/dev/null)" ]; then
+        return 1
+    else
+        return 0
+    fi
+}
+
+_copy=""
+if command_found pbcopy; then
+    _copy="pbcopy "
+elif command_found copy; then
+    _copy="copy "
+fi
+
+if [ ! -z $_copy ]; then
+    printf "[Copy] ssh pub key...\t"
+    eval "$_copy < ~/.ssh/id_rsa.pub"
+    printf "\t\t\t\tDone.\n"
+fi
