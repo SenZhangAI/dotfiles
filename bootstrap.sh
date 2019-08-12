1ae9ce0dc8ac7f95f6db2108724c21202802869e
Sen Zhang <szhang.hust@gmail.com>
2019-08-12 15:47:32 +0800

    better bootstrap message format

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,45 +18,72 @@ system_is() {
     fi
 }
 
+
+GREEN() {
+    printf "\033[32m$1\033[0m"
+}
+
+RED() {
+    printf "\033[31m$1\033[0m"
+}
+
+command_installed() {
+    printf "%-48s" "Check command $1..."
+    found=$(which $1 2>/dev/null)
+    if [ -z "$found" ]; then
+        printf "$(RED "Not Installed")\n"
+        return 1
+    else
+        printf "$(GREEN "Installed")\n"
+        return 0
+    fi
+}
+
+command_not_installed() {
+    printf "%-48s" "Check command $1..."
+    found=$(which $1 2>/dev/null)
+    if [ -z "$found" ]; then
+        printf "$(RED "Not Installed")\n"
+        return 0
+    else
+        printf "$(GREEN "Installed")\n"
+        return 1
+    fi
+}
+
+must_install() {
+    if command_not_installed $1; then
+        printf "\033[33m$1\033[0m need to be installed first!\n"
+        exit 233
+    fi
+}
+
 init_install_config() {
     case $OSTYPE in
         cygwin*)
-            if [[ -z $(which git 2>/dev/null) ]]; then
-                printf "\033[32mgit\033[0m need to be installed first!\n"
-                exit 233
-            fi
-            if [[ -z $(which wget 2>/dev/null) ]]; then
-                printf "\033[32mwget\033[0m need to be installed first!\n"
-                exit 233
-            fi
+            must_install git
+            must_install wget
             ;;
         darwin*)
             #install brew
-            if ! command -v brew >/dev/null 2>&1; then
-                printf "Brew is not installed! Install brew...\n"
+            if command_not_installed brew; then
+                printf "[Install] install brew...\n"
                 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+                printf "$(GREEN "Done")\n"
             fi
 
-            echo "Change mirrors for Homebrew"
+            echo "[Config] change mirrors for Homebrew..."
             #see https://mirrors.tuna.tsinghua.edu.cn/help/homebrew/
-            cd "$(brew --repo)"
-            git remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git
-
-            cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
-            git remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git
-
+            cd "$(brew --repo)" && git remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git
+            cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core" && git remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git
             export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles
+            printf "$(GREEN "Done")\n"
 
             if [ -f $HOME/.local/dotfiles/spec/macOS/.brew-update ]; then
                 export HOMEBREW_NO_AUTO_UPDATE=true
             else
                 brew tap homebrew/cask
-                # Make sure we’re using the latest Homebrew.
-                echo "brew update"
                 brew update
-
-                # Upgrade any already-installed formulae.
-                echo "brew upgrade"
                 brew upgrade
                 brew install git
             fi
@@ -71,7 +98,7 @@ init_install_config() {
             echo 'Server = https://mirrors.tuna.tsinghua.edu.cn/msys2/msys/$arch' > /etc/pacman.d/mirrorlist.msys
 
             pacman -Syu --noconfirm
-            if [ -z $(which git 2>/dev/null) ]; then
+            if command_not_installed git; then
                 pacman -Sy --noconfirm git
             fi
             ;;
@@ -125,9 +152,8 @@ platform_spec_config() {
             cp -rf spec/cygwin/bin/* $BIN/
             _set_go_env_for_cygwin
 
-            if [[ ! -z $(which go 2>/dev/null) ]]; then
+            if command_installed go; then
                 # go is excutable
-                echo go build -o $BIN/git.exe ./spec/cygwin/git.go
                 go build -o git.exe ./spec/cygwin/git.go
                 mv git.exe $BIN/
             fi
@@ -155,6 +181,6 @@ if [ -f $HOME/.bashrc ]; then
     echo "source $ETC/bashrc.sh" >> $HOME/.bashrc
 fi
 
-echo "Install Successfully."
+printf "$(GREEN "Install Successfully")\n"
 
 # vim: sw=4:ts=4
