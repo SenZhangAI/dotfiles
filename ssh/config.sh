c9ca3df681f14f9a14a2d441b29f22ade2eec764
Sen Zhang <szhang.hust@gmail.com>
2019-08-12 15:23:58 +0800

    better print fmt for ssh config

--- ssh/config.sh
+++ ssh/config.sh
@@ -3,44 +3,37 @@
 base_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 source $base_dir/../utils/smart_install.sh
+source $base_dir/../utils/print.sh
 
 if command_not_installed ssh; then
     smart_install openssh
 fi
 
-printf "[Check] if ssh configured?\t"
-
+printf "%-48s" "Check if ssh configured..."
 search=`cat $HOME/.ssh/id_rsa.pub | grep "szhang.hust@gmail.com"`
 
 if [ -z "$search" ]; then
-    printf "\t\t\tNot Configured.\n"
-    echo "[Config] add ssh key..."
+    printf "$(RED "Not Configured")\n"
+
+    printf "%-48s" "[Config] add ssh key..."
     ssh-keygen -t rsa -b 4096 -C "szhang.hust@gmail.com"
     eval $(ssh-agent -s)
     ssh-add ~/.ssh/id_rsa
+    printf "$(GREEN "Done")\n"
 else
-    printf "\t\t\tConfigured.\n"
+    printf "$(GREEN "Configured")\n"
 fi
 
-
-
-command_found() {
-    if [ -z "$(which $1 2>/dev/null)" ]; then
-        return 1
-    else
-        return 0
-    fi
-}
-
 _copy=""
-if command_found pbcopy; then
+if command_installed pbcopy; then
     _copy="pbcopy "
-elif command_found copy; then
+elif command_installed copy; then
     _copy="copy "
 fi
 
 if [ ! -z $_copy ]; then
-    printf "[Copy] ssh pub key...\t"
+    printf "%-48s" "[Copy] ssh pub key..."
     eval "$_copy < ~/.ssh/id_rsa.pub"
-    printf "\t\t\t\tDone.\n"
+    printf "$(GREEN "Done")\n"
 fi
+unset _copy
