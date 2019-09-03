dd3d43c5152397e72b03f9ed3ff0d68219cda909
Sen Zhang <szhang.hust@gmail.com>
2019-09-03 08:42:28 +0800

    using tsinghua's brew bottle mirror, it better then ustc's

--- brew/install.sh
+++ brew/install.sh
@@ -8,7 +8,8 @@ echo "[Config] change mirrors for Homebrew..."
 cd "$(brew --repo)" && git remote set-url origin https://mirrors.ustc.edu.cn/brew.git
 cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core" && git remote set-url origin https://mirrors.ustc.edu.cn/homebrew-core.git
 cd "$(brew --repo)/Library/Taps/homebrew/homebrew-cask" && git remote set-url origin https://mirrors.ustc.edu.cn/homebrew-cask.git
-export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-cask.git
+# export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-cask.git
+export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles
 
 # disable homebrew update, it's too slow!
 export HOMEBREW_NO_AUTO_UPDATE=true
